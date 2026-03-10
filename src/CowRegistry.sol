d6aad32c16a465b1ad9aba4711fe767bec44f330
Edmund Edgar <ed@socialminds.jp>
2026-03-10 03:10:37 +0000

    drop the prefix business

--- src/CowRegistry.sol
+++ src/CowRegistry.sol
@@ -5,84 +5,74 @@ contract CowRegistry {
 
     struct Cow {
         address controller;
-        bytes12 prefix; // set to "did::" to deactivate
         string wrappedDID;
     }
 
+    string constant DEACTIVATED = "did::";
+
     mapping(bytes32 => Cow) public cows;
 
-    event CowInitialized(bytes32 indexed cowHash, address controller, bytes12 prefix, string wrappedDID);
+    event CowInitialized(bytes32 indexed cowHash, address controller, string wrappedDID);
     event CowDeactivated(bytes32 indexed cowHash);
     event ControllerUpdated(bytes32 indexed cowHash, address controller);
-    event WrappedDIDUpdated(bytes32 indexed cowHash, bytes12 prefix, string wrappedDID);
+    event WrappedDIDUpdated(bytes32 indexed cowHash, string wrappedDID);
 
-    function updateWrappedDIDByHash(
-        bytes32 cowHash,
-        bytes12 prefix,
-        string memory wrappedDID
-    ) public {
-        require(cows[cowHash].prefix != bytes12("did::"));
-        require(msg.sender == cows[cowHash].controller);
-
-        require(prefix != bytes12("did::"), "Use deactivate() to deactivate");
-
-        cows[cowHash].prefix = prefix;
-        cows[cowHash].wrappedDID = wrappedDID;
-        emit WrappedDIDUpdated(cowHash, prefix, wrappedDID);
+    function _isDeactivated(bytes32 cowHash) internal view returns (bool) {
+        return keccak256(bytes(cows[cowHash].wrappedDID)) == keccak256(bytes(DEACTIVATED));
     }
 
-    function updateControllerByHash(
-        bytes32 cowHash,
-        address controller
-    ) public {
-        require(cows[cowHash].prefix != bytes12("did::"));
+    function updateWrappedDIDByHash(bytes32 cowHash, string memory wrappedDID) public {
+        require(!_isDeactivated(cowHash));
+        require(msg.sender == cows[cowHash].controller);
+        require(keccak256(bytes(wrappedDID)) != keccak256(bytes(DEACTIVATED)), "Use deactivate() to deactivate");
+
+        cows[cowHash].wrappedDID = wrappedDID;
+        emit WrappedDIDUpdated(cowHash, wrappedDID);
+    }
+
+    function updateControllerByHash(bytes32 cowHash, address controller) public {
+        require(!_isDeactivated(cowHash));
         require(msg.sender == cows[cowHash].controller);
 
         cows[cowHash].controller = controller;
         emit ControllerUpdated(cowHash, controller);
     }
 
-    function calculateCowHash(address controller, bytes12 prefix, string memory wrappedDID) public pure returns (bytes32) {
-        return keccak256(abi.encodePacked(controller, prefix, wrappedDID));
+    function calculateCowHash(address controller, string memory wrappedDID) public pure returns (bytes32) {
+        return keccak256(abi.encodePacked(controller, wrappedDID));
     }
 
-    // Return a cow hash for the settings, storing the cow if it was not already there
-    function _ensureCowInitialized(address controller, bytes12 prefix, string memory wrappedDID) internal returns (bytes32 cowHash) {
-        cowHash = calculateCowHash(controller, prefix, wrappedDID);
-        if (cows[cowHash].prefix == bytes12(0)) { // unregistered
-            cows[cowHash] = Cow(
-                controller,
-                prefix,
-                wrappedDID
-            );
-            emit CowInitialized(cowHash, controller, prefix, wrappedDID);
+    function _ensureCowInitialized(address controller, string memory wrappedDID) internal returns (bytes32 cowHash) {
+        cowHash = calculateCowHash(controller, wrappedDID);
+        if (bytes(cows[cowHash].wrappedDID).length == 0) {
+            cows[cowHash] = Cow(controller, wrappedDID);
+            emit CowInitialized(cowHash, controller, wrappedDID);
         }
         return cowHash;
     }
 
     // You don't particularly need to call this, you can leave it until you make an update
-    function initializeCow(address controller, bytes12 prefix, string memory wrappedDID) external {
-        _ensureCowInitialized(controller, prefix, wrappedDID);
+    function initializeCow(address controller, string memory wrappedDID) external {
+        _ensureCowInitialized(controller, wrappedDID);
     }
 
-    function updateWrappedDID(address controller, bytes12 prefix, string memory wrappedDID, bytes12 newPrefix, string memory newWrappedDID) public {
-        bytes32 cowHash = _ensureCowInitialized(controller, prefix, wrappedDID);
-        updateWrappedDIDByHash(cowHash, newPrefix, newWrappedDID);
+    function updateWrappedDID(address controller, string memory wrappedDID, string memory newWrappedDID) public {
+        bytes32 cowHash = _ensureCowInitialized(controller, wrappedDID);
+        updateWrappedDIDByHash(cowHash, newWrappedDID);
     }
 
-    function updateController(address controller, bytes12 prefix, string memory wrappedDID, address newController) public {
-        bytes32 cowHash = _ensureCowInitialized(controller, prefix, wrappedDID);
+    function updateController(address controller, string memory wrappedDID, address newController) public {
+        bytes32 cowHash = _ensureCowInitialized(controller, wrappedDID);
         updateControllerByHash(cowHash, newController);
     }
 
     function deactivate(bytes32 cowHash) external {
-        require(cows[cowHash].prefix != bytes12("did::"));
+        require(!_isDeactivated(cowHash));
         require(msg.sender == cows[cowHash].controller);
 
-        cows[cowHash].prefix = bytes12("did::");
+        cows[cowHash].wrappedDID = DEACTIVATED;
         cows[cowHash].controller = address(0);
 
         emit CowDeactivated(cowHash);
     }
-
 }
--- test/CowRegistry.t.sol
+++ test/CowRegistry.t.sol
@@ -11,32 +11,22 @@ contract CowRegistryUpdateTest is Test {
     address controller2 = address(0x2222);
 
     // Two did:plc addresses
-    bytes12 plcPrefix = bytes12("did:plc:");
-    string plcDID1    = "7qqsrnkn4moc2jgd"; // did:plc:7qqsrnkn4moc2jgd
-    string plcDID2    = "abcdefghijklmnop"; // did:plc:abcdefghijklmnop
+    string plcDID1 = "did:plc:7qqsrnkn4moc2jgd";
+    string plcDID2 = "did:plc:abcdefghijklmnop";
 
     // did:web addresses of varying lengths
-    bytes12 webPrefix     = bytes12("did:web:");
-    string webDIDShort    = "a.io";            // did:web:a.io
-    string webDIDMedium   = "example.com";     // did:web:example.com
-    string webDIDLong     = "subdomain.very-long-hostname.example.co.uk"; // did:web:subdomain...
-    string webDIDVeryLong = "deep.nested.subdomain.with-a-quite-long-hostname.enterprise.example.com"; // did:web:...
+    string webDIDShort    = "did:web:a.io";
+    string webDIDMedium   = "did:web:example.com";
+    string webDIDLong     = "did:web:subdomain.very-long-hostname.example.co.uk";
+    string webDIDVeryLong = "did:web:deep.nested.subdomain.with-a-quite-long-hostname.enterprise.example.com";
 
     function setUp() public {
         registry = new CowRegistry();
     }
 
-    // -------------------------------------------------------------------------
-    // Helpers
-    // -------------------------------------------------------------------------
-
-    function _hashFor(address ctrl, bytes12 pfx, string memory did) internal view returns (bytes32) {
-        return registry.calculateCowHash(ctrl, pfx, did);
-    }
-
-    function _init(address ctrl, bytes12 pfx, string memory did) internal returns (bytes32) {
-        registry.initializeCow(ctrl, pfx, did);
-        return _hashFor(ctrl, pfx, did);
+    function _init(address ctrl, string memory did) internal returns (bytes32) {
+        registry.initializeCow(ctrl, did);
+        return registry.calculateCowHash(ctrl, did);
     }
 
     // -------------------------------------------------------------------------
@@ -44,28 +34,27 @@ contract CowRegistryUpdateTest is Test {
     // -------------------------------------------------------------------------
 
     function test_updateWrappedDID_plc1_to_plc2() public {
-        bytes32 cowHash = _init(controller1, plcPrefix, plcDID1);
+        bytes32 cowHash = _init(controller1, plcDID1);
 
         vm.prank(controller1);
         uint256 gasBefore = gasleft();
-        registry.updateWrappedDIDByHash(cowHash, plcPrefix, plcDID2);
+        registry.updateWrappedDIDByHash(cowHash, plcDID2);
         uint256 gasUsed = gasBefore - gasleft();
 
-        (, bytes12 pfx, string memory did) = registry.cows(cowHash);
-        assertEq(pfx, plcPrefix);
+        (, string memory did) = registry.cows(cowHash);
         assertEq(did, plcDID2);
         emit log_named_uint("gas updateWrappedDID plc1->plc2", gasUsed);
     }
 
     function test_updateWrappedDID_plc2_to_plc1() public {
-        bytes32 cowHash = _init(controller2, plcPrefix, plcDID2);
+        bytes32 cowHash = _init(controller2, plcDID2);
 
         vm.prank(controller2);
         uint256 gasBefore = gasleft();
-        registry.updateWrappedDIDByHash(cowHash, plcPrefix, plcDID1);
+        registry.updateWrappedDIDByHash(cowHash, plcDID1);
         uint256 gasUsed = gasBefore - gasleft();
 
-        (, , string memory did) = registry.cows(cowHash);
+        (, string memory did) = registry.cows(cowHash);
         assertEq(did, plcDID1);
         emit log_named_uint("gas updateWrappedDID plc2->plc1", gasUsed);
     }
@@ -75,56 +64,55 @@ contract CowRegistryUpdateTest is Test {
     // -------------------------------------------------------------------------
 
     function test_updateWrappedDID_web_short() public {
-        bytes32 cowHash = _init(controller1, plcPrefix, plcDID1);
+        bytes32 cowHash = _init(controller1, plcDID1);
 
         vm.prank(controller1);
         uint256 gasBefore = gasleft();
-        registry.updateWrappedDIDByHash(cowHash, webPrefix, webDIDShort);
+        registry.updateWrappedDIDByHash(cowHash, webDIDShort);
         uint256 gasUsed = gasBefore - gasleft();
 
-        (, bytes12 pfx, string memory did) = registry.cows(cowHash);
-        assertEq(pfx, webPrefix);
+        (, string memory did) = registry.cows(cowHash);
         assertEq(did, webDIDShort);
-        emit log_named_uint("gas updateWrappedDID web short (4 chars)", gasUsed);
+        emit log_named_uint("gas updateWrappedDID web short (12 chars)", gasUsed);
     }
 
     function test_updateWrappedDID_web_medium() public {
-        bytes32 cowHash = _init(controller1, plcPrefix, plcDID1);
+        bytes32 cowHash = _init(controller1, plcDID1);
 
         vm.prank(controller1);
         uint256 gasBefore = gasleft();
-        registry.updateWrappedDIDByHash(cowHash, webPrefix, webDIDMedium);
+        registry.updateWrappedDIDByHash(cowHash, webDIDMedium);
         uint256 gasUsed = gasBefore - gasleft();
 
-        (, , string memory did) = registry.cows(cowHash);
+        (, string memory did) = registry.cows(cowHash);
         assertEq(did, webDIDMedium);
-        emit log_named_uint("gas updateWrappedDID web medium (11 chars)", gasUsed);
+        emit log_named_uint("gas updateWrappedDID web medium (19 chars)", gasUsed);
     }
 
     function test_updateWrappedDID_web_long() public {
-        bytes32 cowHash = _init(controller1, plcPrefix, plcDID1);
+        bytes32 cowHash = _init(controller1, plcDID1);
 
         vm.prank(controller1);
         uint256 gasBefore = gasleft();
-        registry.updateWrappedDIDByHash(cowHash, webPrefix, webDIDLong);
+        registry.updateWrappedDIDByHash(cowHash, webDIDLong);
         uint256 gasUsed = gasBefore - gasleft();
 
-        (, , string memory did) = registry.cows(cowHash);
+        (, string memory did) = registry.cows(cowHash);
         assertEq(did, webDIDLong);
-        emit log_named_uint("gas updateWrappedDID web long (42 chars)", gasUsed);
+        emit log_named_uint("gas updateWrappedDID web long (50 chars)", gasUsed);
     }
 
     function test_updateWrappedDID_web_verylong() public {
-        bytes32 cowHash = _init(controller1, plcPrefix, plcDID1);
+        bytes32 cowHash = _init(controller1, plcDID1);
 
         vm.prank(controller1);
         uint256 gasBefore = gasleft();
-        registry.updateWrappedDIDByHash(cowHash, webPrefix, webDIDVeryLong);
+        registry.updateWrappedDIDByHash(cowHash, webDIDVeryLong);
         uint256 gasUsed = gasBefore - gasleft();
 
-        (, , string memory did) = registry.cows(cowHash);
+        (, string memory did) = registry.cows(cowHash);
         assertEq(did, webDIDVeryLong);
-        emit log_named_uint("gas updateWrappedDID web very long (71 chars)", gasUsed);
+        emit log_named_uint("gas updateWrappedDID web very long (79 chars)", gasUsed);
     }
 
     // -------------------------------------------------------------------------
@@ -132,27 +120,27 @@ contract CowRegistryUpdateTest is Test {
     // -------------------------------------------------------------------------
 
     function test_updateController_plc1() public {
-        bytes32 cowHash = _init(controller1, plcPrefix, plcDID1);
+        bytes32 cowHash = _init(controller1, plcDID1);
 
         vm.prank(controller1);
         uint256 gasBefore = gasleft();
         registry.updateControllerByHash(cowHash, controller2);
         uint256 gasUsed = gasBefore - gasleft();
 
-        (address ctrl, , ) = registry.cows(cowHash);
+        (address ctrl, ) = registry.cows(cowHash);
         assertEq(ctrl, controller2);
         emit log_named_uint("gas updateController plc1 ctrl1->ctrl2", gasUsed);
     }
 
     function test_updateController_plc2() public {
-        bytes32 cowHash = _init(controller2, plcPrefix, plcDID2);
+        bytes32 cowHash = _init(controller2, plcDID2);
 
         vm.prank(controller2);
         uint256 gasBefore = gasleft();
         registry.updateControllerByHash(cowHash, controller1);
         uint256 gasUsed = gasBefore - gasleft();
 
-        (address ctrl, , ) = registry.cows(cowHash);
+        (address ctrl, ) = registry.cows(cowHash);
         assertEq(ctrl, controller1);
         emit log_named_uint("gas updateController plc2 ctrl2->ctrl1", gasUsed);
     }
@@ -162,55 +150,55 @@ contract CowRegistryUpdateTest is Test {
     // -------------------------------------------------------------------------
 
     function test_updateController_web_short() public {
-        bytes32 cowHash = _init(controller1, webPrefix, webDIDShort);
+        bytes32 cowHash = _init(controller1, webDIDShort);
 
         vm.prank(controller1);
         uint256 gasBefore = gasleft();
         registry.updateControllerByHash(cowHash, controller2);
         uint256 gasUsed = gasBefore - gasleft();
 
-        (address ctrl, , ) = registry.cows(cowHash);
+        (address ctrl, ) = registry.cows(cowHash);
         assertEq(ctrl, controller2);
-        emit log_named_uint("gas updateController web short (4 chars)", gasUsed);
+        emit log_named_uint("gas updateController web short (12 chars)", gasUsed);
     }
 
     function test_updateController_web_medium() public {
-        bytes32 cowHash = _init(controller1, webPrefix, webDIDMedium);
+        bytes32 cowHash = _init(controller1, webDIDMedium);
 
         vm.prank(controller1);
         uint256 gasBefore = gasleft();
         registry.updateControllerByHash(cowHash, controller2);
         uint256 gasUsed = gasBefore - gasleft();
 
-        (address ctrl, , ) = registry.cows(cowHash);
+        (address ctrl, ) = registry.cows(cowHash);
         assertEq(ctrl, controller2);
-        emit log_named_uint("gas updateController web medium (11 chars)", gasUsed);
+        emit log_named_uint("gas updateController web medium (19 chars)", gasUsed);
     }
 
     function test_updateController_web_long() public {
-        bytes32 cowHash = _init(controller1, webPrefix, webDIDLong);
+        bytes32 cowHash = _init(controller1, webDIDLong);
 
         vm.prank(controller1);
         uint256 gasBefore = gasleft();
         registry.updateControllerByHash(cowHash, controller2);
         uint256 gasUsed = gasBefore - gasleft();
 
-        (address ctrl, , ) = registry.cows(cowHash);
+        (address ctrl, ) = registry.cows(cowHash);
         assertEq(ctrl, controller2);
-        emit log_named_uint("gas updateController web long (42 chars)", gasUsed);
+        emit log_named_uint("gas updateController web long (50 chars)", gasUsed);
     }
 
     function test_updateController_web_verylong() public {
-        bytes32 cowHash = _init(controller1, webPrefix, webDIDVeryLong);
+        bytes32 cowHash = _init(controller1, webDIDVeryLong);
 
         vm.prank(controller1);
         uint256 gasBefore = gasleft();
         registry.updateControllerByHash(cowHash, controller2);
         uint256 gasUsed = gasBefore - gasleft();
 
-        (address ctrl, , ) = registry.cows(cowHash);
+        (address ctrl, ) = registry.cows(cowHash);
         assertEq(ctrl, controller2);
-        emit log_named_uint("gas updateController web very long (71 chars)", gasUsed);
+        emit log_named_uint("gas updateController web very long (79 chars)", gasUsed);
     }
 
     // -------------------------------------------------------------------------
@@ -218,15 +206,15 @@ contract CowRegistryUpdateTest is Test {
     // -------------------------------------------------------------------------
 
     function test_updateWrappedDID_rejectsNonController() public {
-        bytes32 cowHash = _init(controller1, plcPrefix, plcDID1);
+        bytes32 cowHash = _init(controller1, plcDID1);
 
         vm.prank(controller2);
         vm.expectRevert();
-        registry.updateWrappedDIDByHash(cowHash, plcPrefix, plcDID2);
+        registry.updateWrappedDIDByHash(cowHash, plcDID2);
     }
 
     function test_updateController_rejectsNonController() public {
-        bytes32 cowHash = _init(controller1, plcPrefix, plcDID1);
+        bytes32 cowHash = _init(controller1, plcDID1);
 
         vm.prank(controller2);
         vm.expectRevert();
@@ -234,13 +222,13 @@ contract CowRegistryUpdateTest is Test {
     }
 
     function test_updateWrappedDID_rejectsAfterDeactivation() public {
-        bytes32 cowHash = _init(controller1, plcPrefix, plcDID1);
+        bytes32 cowHash = _init(controller1, plcDID1);
 
         vm.prank(controller1);
         registry.deactivate(cowHash);
 
         vm.prank(address(0));
         vm.expectRevert();
-        registry.updateWrappedDIDByHash(cowHash, plcPrefix, plcDID2);
+        registry.updateWrappedDIDByHash(cowHash, plcDID2);
     }
 }
