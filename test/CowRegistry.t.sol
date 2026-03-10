27a12b35b81b312079f46e0c5f9336c7dcb2dbc1
Edmund Edgar <ed@socialminds.jp>
2026-03-10 03:36:22 +0000

    use normal forge tooling for gas estimation

--- test/CowRegistry.t.sol
+++ test/CowRegistry.t.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.0;
+pragma solidity 0.8.28;
 
 import "forge-std/Test.sol";
 import "../src/CowRegistry.sol";
@@ -37,26 +37,20 @@ contract CowRegistryUpdateTest is Test {
         bytes32 cowHash = _init(controller1, plcDID1);
 
         vm.prank(controller1);
-        uint256 gasBefore = gasleft();
         registry.updateWrappedDIDByHash(cowHash, plcDID2);
-        uint256 gasUsed = gasBefore - gasleft();
 
         (, string memory did) = registry.cows(cowHash);
         assertEq(did, plcDID2);
-        emit log_named_uint("gas updateWrappedDID plc1->plc2", gasUsed);
     }
 
     function test_updateWrappedDID_plc2_to_plc1() public {
         bytes32 cowHash = _init(controller2, plcDID2);
 
         vm.prank(controller2);
-        uint256 gasBefore = gasleft();
         registry.updateWrappedDIDByHash(cowHash, plcDID1);
-        uint256 gasUsed = gasBefore - gasleft();
 
         (, string memory did) = registry.cows(cowHash);
         assertEq(did, plcDID1);
-        emit log_named_uint("gas updateWrappedDID plc2->plc1", gasUsed);
     }
 
     // -------------------------------------------------------------------------
@@ -67,52 +61,40 @@ contract CowRegistryUpdateTest is Test {
         bytes32 cowHash = _init(controller1, plcDID1);
 
         vm.prank(controller1);
-        uint256 gasBefore = gasleft();
         registry.updateWrappedDIDByHash(cowHash, webDIDShort);
-        uint256 gasUsed = gasBefore - gasleft();
 
         (, string memory did) = registry.cows(cowHash);
         assertEq(did, webDIDShort);
-        emit log_named_uint("gas updateWrappedDID web short (12 chars)", gasUsed);
     }
 
     function test_updateWrappedDID_web_medium() public {
         bytes32 cowHash = _init(controller1, plcDID1);
 
         vm.prank(controller1);
-        uint256 gasBefore = gasleft();
         registry.updateWrappedDIDByHash(cowHash, webDIDMedium);
-        uint256 gasUsed = gasBefore - gasleft();
 
         (, string memory did) = registry.cows(cowHash);
         assertEq(did, webDIDMedium);
-        emit log_named_uint("gas updateWrappedDID web medium (19 chars)", gasUsed);
     }
 
     function test_updateWrappedDID_web_long() public {
         bytes32 cowHash = _init(controller1, plcDID1);
 
         vm.prank(controller1);
-        uint256 gasBefore = gasleft();
         registry.updateWrappedDIDByHash(cowHash, webDIDLong);
-        uint256 gasUsed = gasBefore - gasleft();
 
         (, string memory did) = registry.cows(cowHash);
         assertEq(did, webDIDLong);
-        emit log_named_uint("gas updateWrappedDID web long (50 chars)", gasUsed);
     }
 
     function test_updateWrappedDID_web_verylong() public {
         bytes32 cowHash = _init(controller1, plcDID1);
 
         vm.prank(controller1);
-        uint256 gasBefore = gasleft();
         registry.updateWrappedDIDByHash(cowHash, webDIDVeryLong);
-        uint256 gasUsed = gasBefore - gasleft();
 
         (, string memory did) = registry.cows(cowHash);
         assertEq(did, webDIDVeryLong);
-        emit log_named_uint("gas updateWrappedDID web very long (79 chars)", gasUsed);
     }
 
     // -------------------------------------------------------------------------
@@ -123,26 +105,20 @@ contract CowRegistryUpdateTest is Test {
         bytes32 cowHash = _init(controller1, plcDID1);
 
         vm.prank(controller1);
-        uint256 gasBefore = gasleft();
         registry.updateControllerByHash(cowHash, controller2);
-        uint256 gasUsed = gasBefore - gasleft();
 
         (address ctrl, ) = registry.cows(cowHash);
         assertEq(ctrl, controller2);
-        emit log_named_uint("gas updateController plc1 ctrl1->ctrl2", gasUsed);
     }
 
     function test_updateController_plc2() public {
         bytes32 cowHash = _init(controller2, plcDID2);
 
         vm.prank(controller2);
-        uint256 gasBefore = gasleft();
         registry.updateControllerByHash(cowHash, controller1);
-        uint256 gasUsed = gasBefore - gasleft();
 
         (address ctrl, ) = registry.cows(cowHash);
         assertEq(ctrl, controller1);
-        emit log_named_uint("gas updateController plc2 ctrl2->ctrl1", gasUsed);
     }
 
     // -------------------------------------------------------------------------
@@ -153,52 +129,40 @@ contract CowRegistryUpdateTest is Test {
         bytes32 cowHash = _init(controller1, webDIDShort);
 
         vm.prank(controller1);
-        uint256 gasBefore = gasleft();
         registry.updateControllerByHash(cowHash, controller2);
-        uint256 gasUsed = gasBefore - gasleft();
 
         (address ctrl, ) = registry.cows(cowHash);
         assertEq(ctrl, controller2);
-        emit log_named_uint("gas updateController web short (12 chars)", gasUsed);
     }
 
     function test_updateController_web_medium() public {
         bytes32 cowHash = _init(controller1, webDIDMedium);
 
         vm.prank(controller1);
-        uint256 gasBefore = gasleft();
         registry.updateControllerByHash(cowHash, controller2);
-        uint256 gasUsed = gasBefore - gasleft();
 
         (address ctrl, ) = registry.cows(cowHash);
         assertEq(ctrl, controller2);
-        emit log_named_uint("gas updateController web medium (19 chars)", gasUsed);
     }
 
     function test_updateController_web_long() public {
         bytes32 cowHash = _init(controller1, webDIDLong);
 
         vm.prank(controller1);
-        uint256 gasBefore = gasleft();
         registry.updateControllerByHash(cowHash, controller2);
-        uint256 gasUsed = gasBefore - gasleft();
 
         (address ctrl, ) = registry.cows(cowHash);
         assertEq(ctrl, controller2);
-        emit log_named_uint("gas updateController web long (50 chars)", gasUsed);
     }
 
     function test_updateController_web_verylong() public {
         bytes32 cowHash = _init(controller1, webDIDVeryLong);
 
         vm.prank(controller1);
-        uint256 gasBefore = gasleft();
         registry.updateControllerByHash(cowHash, controller2);
-        uint256 gasUsed = gasBefore - gasleft();
 
         (address ctrl, ) = registry.cows(cowHash);
         assertEq(ctrl, controller2);
-        emit log_named_uint("gas updateController web very long (79 chars)", gasUsed);
     }
 
     // -------------------------------------------------------------------------
